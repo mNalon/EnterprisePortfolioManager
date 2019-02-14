16769dc7888508d5d8c272dd2b5655b10a9c3e72
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2019-02-14 01:25:50 -0200

    Unsubscribe to user load event when app component is destroyed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { User } from '../shared/models/user';
 import { UserService } from '../shared/services/user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,16 @@ import { UserService } from '../shared/services/user.service';
 })
 export class AppComponent implements OnInit, OnDestroy {
   loggedUser: User = null;
+  loadUserSubscription: Subscription;
 
   constructor(private userService: UserService) {}
 
   ngOnInit() {
-    this.userService.userLoaded.subscribe(user => { this.loggedUser = user; });
+    this.loadUserSubscription = this.userService.userLoaded.subscribe(user => { this.loggedUser = user; });
     this.userService.userSessionInfo().subscribe(user => { this.userService.loadUserInfo(user); });
   }
 
   ngOnDestroy() {
-    // TODO: unsubscribe all observers
+    this.loadUserSubscription.unsubscribe();
   }
 }
